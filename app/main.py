be270110773e451b3ab14c5e18c4ab561281e429
Tanimal <bobcheng.bob@gmail.com>
2025-05-15 10:33:04 +0800

    update api functions

--- app/main.py
+++ app/main.py
@@ -17,41 +17,56 @@ async def run_proxy():
     await m.run()
 
 def control():
+
+    print("Welcome to Trumproxy Control CLI.")
+    help_message = """usage:
+            rules: show all tariff rules
+            retain: show all retained traffic
+            set <country> <rate> <is_dropped>: set tariff rule for a country
+            remove <country>: remove tariff rule for a country
+            exit: exit the control CLI
+            help: show this help message"""
+    print(help_message)
+
     while True:
         cmd = input("Enter command: ").strip()
         
-        if cmd == "get tariff":
-            map = proxy_instance.get_tariff_map()
-            for k, v in map.items():
-                print(f"{k}: {v}%")
+        if cmd == "rules":
+            map = proxy_instance.get_tariff_rules()
+            for country, rule in map.items():
+                print(f"{country}: {rule.rate}%, dropped: {rule.dropped}")
         
-        if cmd == "get retain":
+        elif cmd == "retain":
             traffics = proxy_instance.get_retain_traffic()
-            for k, v in traffics.items():
-                print(f"{k}: {v}")
+            for id, traffic in traffics.items():
+                print(f"{id}: {traffic.request_url}, size: {traffic.size} bytes, from: {traffic.from_ip}, to: {traffic.to_client_ip}, rtt: {traffic.rtt_time} ms, retain time: {traffic.retain_time} s")
 
         elif cmd.startswith("set"):
             try:
-                _, country, tariff = cmd.split()
-                print(f"Set {tariff}% tariff on {country}.")
-                proxy_instance.set_tariff_for_country(country, int(tariff))
+                _, country, rate, is_droppped = cmd.split()
+                proxy_instance.set_tariff_rule(
+                    country,
+                    float(rate),
+                    is_droppped.lower() == "true",
+                )
+                print(f"Set {country} tariff to {rate}%, dropped: {is_droppped}")
             except:
                 print("Invalid usage.")
 
         elif cmd.startswith("remove"):
             try:
                 _, country = cmd.split()
-                print(f"Remove tariff on {country}.")
-                proxy_instance.remove_tariff_for_country(country)
+                proxy_instance.remove_tariff_rule(country)
+                print(f"Removed {country} tariff")
             except:
                 print("Invalid usage.")
         
         elif cmd == "exit":
             print("Exiting...")
             break
-        
-        else:
-            print("Unknown command.")
+
+        elif cmd == "help":
+            print(help_message)
 
 
 if __name__ == "__main__":
--- app/trumproxy.py
+++ app/trumproxy.py
@@ -1,8 +1,10 @@
 import logging
+import asyncio
+from dataclasses import dataclass
+import time
 from mitmproxy import http
 import geoip2.database
 import geoip2.errors
-import asyncio
 
 # logging.basicConfig(filename='trumproxy.log', level=logging.info,
 #                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -10,63 +12,157 @@ import asyncio
 GEOIP_DB_PATH = "./GeoLite2-Country.mmdb"
 
 
+@dataclass
+class TariffRule:
+    rate: int
+    """
+    The rate of the tariff to calculate retain_time.\n
+    retain_time = rtt_time * (rate / 100).\n
+    0-100.
+    """
+
+    dropped: bool
+    """
+    To drop the traffic or not. If True, the rate is ignored.
+    """
+
+@dataclass
+class RetainedResponse:
+    """
+    A retained response.
+    """
+
+    request_url: str
+    """
+    The url being request.
+    """
+    
+    size: int
+    """
+    The size of the response.
+    """
+    
+    from_ip: str
+    """
+    The IP address of the server.
+    """
+
+    from_country_code: str
+    """
+    The country code of the server.
+    """
+
+    to_client_ip: str
+    """
+    The IP address of the client.
+    """
+    
+    recv_time: float
+    """
+    The time when the response was received.
+    """
+    
+    rtt_time: float
+    """
+    The round trip time of the request.
+    """
+
+    retain_time: float
+    """
+    The time to retain the response.
+    """
+
+
 
 class TrumproxyAddon:
     def __init__(self):
-        self.tariff_map = {}
-        self.retain_traffic = {}
+        self.tariff_rules: dict[str, TariffRule] = {}
+        self.retain_responses: dict[str, RetainedResponse] = {}
+
         self.geo_identifier = geoip2.database.Reader(GEOIP_DB_PATH)
         self.f = open("trumproxy.log", "w", 1)
 
-    def request(self, flow: http.HTTPFlow):
-        packet_url = flow.request.pretty_url
-        dst_ip = flow.server_conn.peername[0]
-
+    def response(self, flow: http.HTTPFlow):
         try:
-            dst_location = self.geo_identifier.country(dst_ip)
-            iso_code = dst_location.country.iso_code
+            from_ip = flow.server_conn.peername[0]
+            country = self.geo_identifier.country(from_ip)
+            from_country_code = country.country.iso_code
             
-            self.f.write(f"[PASS] {packet_url} from {dst_ip} (country: {iso_code})\n")
+            if from_country_code is None:
+                self.f.write(f"[ERROR] No country code for {from_ip}\n")
+                return
 
-            if iso_code in self.tariff_map:
-                tariff = self.tariff_map[iso_code]
-                self.f.write(f"[TARIFF] {dst_ip} tariffed for {tariff}s\n")
-                asyncio.create_task(self.tariffed_resume(flow, tariff))
+            if from_country_code not in self.tariff_rules:
+                self.f.write(f"[PASS] No tariff for {from_ip} (country: {from_country_code})\n")
+                return
+            
 
-                self.retain_traffic[flow.id] = {
-                    "url": packet_url,
-                    "ip": dst_ip,
-                    "country": iso_code,
-                }
+            rule: TariffRule = self.tariff_rules[from_country_code]
 
-                flow.intercept()
+            if rule.dropped:
+                self.f.write(f"[DROP] {from_ip} (country: {from_country_code})\n")
+                flow.kill()
+                return
+
+            rtt_time = flow.response.timestamp_end - flow.request.timestamp_start
+            retain_time = rtt_time * (rule.rate / 100)
+            self.f.write(f"[RETAIN] {from_ip} (country: {from_country_code}) for {retain_time}ms\n")
+
+            self.retain_responses[flow.id] = RetainedResponse(
+                request_url=flow.request.pretty_url,
+                size=len(flow.response.content),
+                from_ip=from_ip,
+                from_country_code=from_country_code,
+                to_client_ip=flow.client_conn.peername[0],
+                recv_time=flow.response.timestamp_end,
+                rtt_time=rtt_time,
+                retain_time=retain_time,
+            )
+            
+            asyncio.create_task(self.tariffed_resume(flow, retain_time))
+            flow.intercept()
 
         except Exception as e:
-            logging.error(f"Error: {e}")
+            self.f.write(f"[Error] {e}")
 
     def done(self):
         self.f.close()
 
-    async def tariffed_resume(self, flow: http.HTTPFlow, tariff: int):
-        await asyncio.sleep(tariff)
+    async def retain_flow(self, flow: http.HTTPFlow, retain_time: float):
+        await asyncio.sleep(retain_time)
         if not flow.response:
-            self.retain_traffic.pop(flow.id, None)
+            self.retain_responses.pop(flow.id, None)
             flow.resume()
 
     # for API control
-    def get_tariff_map(self):
-        return self.tariff_map
+    def get_tariff_rules(self) -> dict[str, TariffRule]:
+        """
+        Get all tariff rules.
+        """
+        return self.tariff_rules
     
-    def get_retain_traffic(self):
-        return self.retain_traffic
+    def get_retain_traffic(self) -> dict[str, RetainedResponse]:
+        """
+        Get the retained traffic.
+        """
+        return self.retain_responses
 
-    def set_tariff_for_country(self, iso_code, tariff):
-        self.tariff_map[iso_code] = tariff
+    def set_tariff_rule(self, iso_code, tariff, dropped=False):
+        """
+        Set the tariff rule for the given country code.
+        :param iso_code: The ISO 3166-1 alpha-2 country code.
+        :param tariff: The tariff rate (0-100).
+        :param dropped: Whether to drop the traffic or not.
+        """
+        self.tariff_rules.setdefault(iso_code, TariffRule(rate=tariff, dropped=dropped))
 
-    def remove_tariff_for_country(self, iso_code):
-        if iso_code in self.tariff_map:
-            self.tariff_map.pop(iso_code)
+    def remove_tariff_rule(self, iso_code):
+        """
+        Remove the tariff rule for the given country code.
+        :param iso_code: The ISO 3166-1 alpha-2 country code.
+        """
+        if iso_code in self.tariff_rules:
+            self.tariff_rules.pop(iso_code)
 
 
 proxy_instance = TrumproxyAddon()
-
